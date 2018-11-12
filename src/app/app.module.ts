ce0985aa5b8b4e0694316f3cbee42ad1436a7316
ARDbones <riccardo.ardossi@soprasteria.com>
2018-11-12 17:06:29 +0100

    Integrazione appunti JS e sistemazioni stilistiche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -22,7 +22,7 @@ import { JavascriptComponent } from './javascript/javascript.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [Title],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/javascript/javascript.component.ts
+++ src/app/javascript/javascript.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-javascript',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JavascriptComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title) {
+    titleService.setTitle('JavaScript - Interactive Notes');
+  }
 
   ngOnInit() {
   }
